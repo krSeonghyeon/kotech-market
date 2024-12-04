965f807b7ccd7467d3da281fd94bf13c70f3ee20
krSeonghyeon <ya41921174@gmail.com>
2024-12-04 21:40:59 +0900

    chore: 댓글 관련 부분 삭제

--- src/main/java/com/koreatech/market/repository/CommentRepository.java
+++
@@ -1,8 +0,0 @@
-package com.koreatech.market.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.koreatech.market.domain.Comment;
-
-public interface CommentRepository extends JpaRepository<Comment, Long> {
-}
